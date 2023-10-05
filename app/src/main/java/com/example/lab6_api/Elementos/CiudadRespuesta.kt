05aafe445eed06b8d3500d548f4bd2f306333d47
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-10-05 10:08:18 -0600

    Update CiudadRespuesta.kt

--- app/src/main/java/com/example/lab6_api/Elementos/CiudadRespuesta.kt
+++ app/src/main/java/com/example/lab6_api/Elementos/CiudadRespuesta.kt
@@ -1,11 +1,17 @@
 package com.example.lab6_api.Elementos
 
 import com.google.gson.annotations.SerializedName
-
+/**
+ * Clase CiudadRespuesta
+ *
+ * Clase que defina el objeto respuesta que se obtendrá de la llamada para mapear la estructura del JSON que la API devuelve
+ * */
 class CiudadRespuesta {
+    // Mapeo de la clave _links del JSON de la API
     @SerializedName("_links")
     var links: Links? = null
 
+    // Mapeo de la clave ua:item del JSON de la API
     class Links {
         @SerializedName("ua:item")
         var cities: ArrayList<Ciudad>? = null
